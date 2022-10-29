e623b40f4bdea2e0682fa443fd341156a86b43c9
ibozorus <korkmazie@gmail.com>
2022-10-29 11:33:37 +0200

    - POST-Request

--- src/main/java/com/ex/youtubekurs/student/StudentService.java
+++ src/main/java/com/ex/youtubekurs/student/StudentService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -21,6 +22,10 @@ public class StudentService {
     }
 
     public Student saveStudent(Student student) {
+        Optional<Student> studentByEmail = studentRepository.findStudentByEmail(student.getEmail());
+        if (studentByEmail.isPresent()) {
+            throw new IllegalStateException("Email Taken");
+        }
         return studentRepository.save(student);
     }
 }
